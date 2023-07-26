50b769b33439dd542ac9113c5a13eb699b95fee0
Jacob <jacobwroberts33@gmail.com>
2023-07-26 12:04:22 -0400

    pass all tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,28 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 11
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    new_nums = list()
+    for num in int_list:
+        new_num = num * num
+        new_nums.append(new_num)
+    return new_nums
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i < 101:
+        if (i % 3 == 0) and (i % 5 == 0):
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+        i += 1
